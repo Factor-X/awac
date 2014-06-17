03036d07ad19d1ac7af13b3fa122814b4710a500
Gaetan Paucot <gaetan.paucot@factorx.eu>
2014-06-17 12:57:57 +0200

    Base package with a dummy example

--- app/controllers/BaseController.java
+++
@@ -1,7 +0,0 @@
-package controllers;
-
-/**
- * Created by gaetan on 6/16/14.
- */
-public class BaseController {
-}
--- app/dto/DTO.java
+++
@@ -1,7 +0,0 @@
-package dto;
-
-/**
- * Created by gaetan on 6/16/14.
- */
-public class DTO {
-}
