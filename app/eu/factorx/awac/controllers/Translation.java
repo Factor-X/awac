78918b3bcb7b39150451690b47128e44731cf791
Gaetan Paucot <gaetan.paucot@factorx.eu>
2014-06-17 14:18:01 +0200

    Ajax translations from static source with loading screen

--- app/eu/factorx/awac/controllers/Translation.java
+++ app/eu/factorx/awac/controllers/Translation.java
@@ -1,7 +1,12 @@
 package eu.factorx.awac.controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by gaetan on 6/17/14.
@@ -10,7 +15,16 @@ import play.mvc.Result;
 public class Translation extends Controller {
 
     public static Result fetch(String language) {
-        return ok("{ \"ABCD\": \"Enorme bravo !\" }");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+
+        Map<String, String> map = new HashMap<>();
+
+        map.put("ABCD", "Deux poules discutent:<br/>- Comment vas-tu ma cocotte?<br/>- Pas très bien. Je crois que je couve quelque chose !");
+
+        return ok(Json.toJson(map));
     }
 
 }
