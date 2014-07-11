9257e497e91c9fef4f763d02b17892e06871eca5
jcarton <jerome.carton@factorx.eu>
2014-07-11 12:53:15 +0200

    Added finders for ScopeService

--- app/eu/factorx/awac/service/ScopeService.java
+++ app/eu/factorx/awac/service/ScopeService.java
@@ -1,7 +1,16 @@
 package eu.factorx.awac.service;
 
+import eu.factorx.awac.models.business.Organization;
+import eu.factorx.awac.models.business.Product;
 import eu.factorx.awac.models.business.Scope;
+import eu.factorx.awac.models.business.Site;
 
 public interface ScopeService extends PersistenceService<Scope> {
+
+	Scope findBySite(Site site);
+
+	Scope findByOrganization(Organization organization);
+
+	Scope findByProduct(Product product);
 
 }
