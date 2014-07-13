b8620d322c843ff10805a86dd5ab078b47a097e5
jcarton <jerome.carton@factorx.eu>
2014-07-13 07:24:27 +0200

    Removed unused methods from UnitService

--- app/eu/factorx/awac/service/UnitService.java
+++ app/eu/factorx/awac/service/UnitService.java
@@ -1,13 +1,7 @@
 package eu.factorx.awac.service;
 
-import java.util.List;
-
 import eu.factorx.awac.models.knowledge.Unit;
 
 public interface UnitService extends PersistenceService<Unit> {
-
-	Unit findBySymbol(String symbol);
-
-	List<String> findAllSymbols();
 
 }
