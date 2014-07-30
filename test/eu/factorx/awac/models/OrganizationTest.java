645d20e21ae6c83d96b1d1e3b1c5ad9524c53e38
gaston-hollands <gaston.hollands@factorx.eu>
2014-07-30 18:06:47 +0200

    Organizaion test draft

--- test/eu/factorx/awac/models/OrganizationTest.java
+++ test/eu/factorx/awac/models/OrganizationTest.java
@@ -4,22 +4,21 @@ import eu.factorx.awac.models.account.Administrator;
 import eu.factorx.awac.models.business.Organization;
 import org.hibernate.Session;
 import org.junit.Test;
+import play.api.Play;
 import play.db.jpa.JPA;
 import play.db.jpa.Transactional;
 import play.libs.F;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 
 public class OrganizationTest extends BaseModelTest {
 
-    /* ADMINISTRATOR TESTS */
-//    @Transactional
+    /* ORGANIZATION TESTS */
+
     @Test
     public void createOrganization() {
 
-        assertEquals(1, 1);
         JPA.withTransaction(new F.Callback0() {
             @Override
             public void invoke() throws Throwable {
@@ -27,25 +26,102 @@ public class OrganizationTest extends BaseModelTest {
                 JPA.em().unwrap(Session.class).flush();
             }
         });
-//        new Administrator("admin001id", "admin001pwd", "admin001name","admin001lastname").save();
-//        Administrator admin = Administrator.find.where().eq("identifier", "admin001id").findUnique();
-//        assertNotNull(admin);
-//        assertEquals(admin.lastname, "admin001name");
     } // end of test method
 
 
     @Test
-    public void retrieveOrganization() {
-        assertEquals(1, 1);
+    public void retrieveOrganizationSuccess() {
         JPA.withTransaction(new F.Callback0() {
             @Override
             public void invoke() throws Throwable {
                 String query = "select o from Organization o where o.name = 'factorx'";
-                System.out.print("getResultList().get(0)-->"+JPA.em().createQuery(query).getResultList().get(0));
+//                System.out.print("getResultList().get(0)-->"+JPA.em().createQuery(query).getResultList().get(0));
                 Organization org = JPA.em().createQuery(query,Organization.class).getResultList().get(0);
-                System.out.print("organization name = " + org.getName());
+//                System.out.print("organization name = " + org.getName());
+                assertEquals(org.getName(), "factorx");
             }
         });
     }
+
+    @Test
+    public void retrieveOrganizationFailure() {
+        JPA.withTransaction(new F.Callback0() {
+            @Override
+            public void invoke() throws Throwable {
+                Organization org = null;
+                String query = "select o from Organization o where o.name = 'factor'";
+//                System.out.print("getResultList().get(0)-->"+JPA.em().createQuery(query).getResultList().get(0));
+                try {
+                    org = JPA.em().createQuery(query, Organization.class).getResultList().get(0);
+                } catch (Exception empty) {}
+              assertNull(org);
+            }
+        });
+    }
+
+    //@Test
+    public void deleteOrganization() {
+        JPA.withTransaction(new F.Callback0() {
+            @Override
+            public void invoke() throws Throwable {
+                Organization org = null;
+                String query = "select o from Organization o where o.name = 'factorx'";
+//                System.out.print("getResultList().get(0)-->"+JPA.em().createQuery(query).getResultList().get(0));
+                try {
+                    org = JPA.em().createQuery(query, Organization.class).getResultList().get(0);
+                } catch (Exception empty) {}
+                assertNull(org);
+                try {
+                  JPA.em().remove(org);
+                } catch (Exception empty) {}
+
+            }
+        });
+    }
+
+
+    //@Test
+    public void updateOrganization() {
+
+
+        // Create
+        JPA.withTransaction(new F.Callback0() {
+            @Override
+            public void invoke() throws Throwable {
+                JPA.em().persist(new Organization("carrefour"));
+                JPA.em().unwrap(Session.class).flush();
+            }
+        });
+
+        // Check persistence and update name
+        JPA.withTransaction(new F.Callback0() {
+            @Override
+            public void invoke() throws Throwable {
+                // Check persistence
+                String query = "select o from Organization o where o.name = 'carrefour'";
+                Organization org = JPA.em().createQuery(query,Organization.class).getResultList().get(0);
+                assertEquals(org.getName(), "carrefour");
+                org.setName("CarrefourWithUpperCase");
+                JPA.em().persist(org);
+                JPA.em().unwrap(Session.class).flush();
+            }
+        });
+
+        // Check persistence and update name
+        JPA.withTransaction(new F.Callback0() {
+            @Override
+            public void invoke() throws Throwable {
+                // Check persistence
+                String query = "select o from Organization o where o.name = 'CarrefourWithUpperCase'";
+                Organization org = JPA.em().createQuery(query,Organization.class).getResultList().get(0);
+                // name has changed
+                assertEquals(org.getName(), "CarrefourWithUpperCase");
+                // list only contains 1 organisation
+                assertEquals(1,JPA.em().createQuery(query,Organization.class).getResultList().size());
+            }
+        });
+
+
+    } // end of test method
 
 }
