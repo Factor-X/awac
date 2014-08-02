425f00f1828962e69dd1c6d81509e7d9cb169f10
jcarton <jerome.carton@factorx.eu>
2014-08-02 13:05:11 +0200

    Added a custom cache on UnitCategoryService

--- app/eu/factorx/awac/service/impl/UnitCategoryServiceImpl.java
+++ app/eu/factorx/awac/service/impl/UnitCategoryServiceImpl.java
@@ -1,11 +1,25 @@
 package eu.factorx.awac.service.impl;
 
-import eu.factorx.awac.models.knowledge.UnitCategory;
-import eu.factorx.awac.service.UnitCategoryService;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
+import eu.factorx.awac.models.knowledge.UnitCategory;
+import eu.factorx.awac.service.UnitCategoryService;
+
 @Component
-public class UnitCategoryServiceImpl extends AbstractJPAPersistenceServiceImpl<UnitCategory> implements
-		UnitCategoryService {
+public class UnitCategoryServiceImpl extends AbstractJPAPersistenceServiceImpl<UnitCategory> implements UnitCategoryService {
+
+	// custom cache! unit and unit categories are "strictly" read-only data for now...
+	// TODO Improve this
+	private static List<UnitCategory> allCategories = null;
+
+	@Override
+	public List<UnitCategory> findAll() {
+		if (allCategories == null) {
+			allCategories = super.findAll();
+		}
+		return allCategories;
+	}
 
 }
