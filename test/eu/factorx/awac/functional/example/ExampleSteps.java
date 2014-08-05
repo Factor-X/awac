1d47483137b71e1aa99a0475414004f72da6f268
gaston.hollands <gaston.hollands@factorx.eu>
2014-08-05 18:26:39 +0200

    - Adding Dependency Injection with Guice for cucumber - Setting up first TestServer and TestBrowser in cucumber.

--- test/eu/factorx/awac/functional/example/ExampleSteps.java
+++ test/eu/factorx/awac/functional/example/ExampleSteps.java
@@ -1,23 +1,38 @@
 package eu.factorx.awac.functional.example;
 
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import eu.factorx.awac.functional.hooks.GlobalHooks;
-
+import play.test.TestBrowser;
+import static org.fest.assertions.Assertions.assertThat;
 
 public class ExampleSteps {
+	@Inject
+	private TestBrowser testBrowser;
+
+	@Inject
+	@Named("PORT")
+	private Integer port;
+
+	@Inject
+	@Named("DOMAIN")
+	private String domain;
+
 	@Given("^I have setup Play$")
 	public void I_have_setup_Play() throws Throwable {
 	}
 
 	@When("^I go to the landing page$")
 	public void I_go_to_the_landing_page() throws Throwable {
-		//GlobalHooks.TEST_BROWSER.goTo("http://localhost:" + GlobalHooks.PORT + "/awac/");
+		play.Logger.info("HTTP Goto : " + "http://localhost:" + port + "/" + domain + "/");
+		testBrowser.goTo("http://localhost:" + port + "/" + domain + "/tests");
+		play.Logger.info("Page Source : " + testBrowser.pageSource());
 	}
 
 	@Then("^the title should be \"([^\"]*)\"$")
 	public void the_title_should_be(String title) throws Throwable {
-		//assertThat(GlobalHooks.TEST_BROWSER.title()).isEqualTo(title);
+		assertThat(testBrowser.pageSource()).contains(title);
 	}
 }
