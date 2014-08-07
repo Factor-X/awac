dc803b591583c42e0f7e5e3ff8aea6a6b328a917
jcarton <jerome.carton@factorx.eu>
2014-08-07 16:18:54 +0200

    Added a findByCode(PeriodCode) method in PeriodService

--- app/eu/factorx/awac/service/impl/PeriodServiceImpl.java
+++ app/eu/factorx/awac/service/impl/PeriodServiceImpl.java
@@ -1,9 +1,24 @@
 package eu.factorx.awac.service.impl;
 
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Component;
+
+import play.db.jpa.JPA;
+import eu.factorx.awac.models.code.type.PeriodCode;
 import eu.factorx.awac.models.knowledge.Period;
 import eu.factorx.awac.service.PeriodService;
-import org.springframework.stereotype.Component;
 
 @Component
 public class PeriodServiceImpl extends AbstractJPAPersistenceServiceImpl<Period> implements PeriodService {
+
+	@Override
+	public Period findByCode(PeriodCode periodCode) {
+		Session session = JPA.em().unwrap(Session.class);
+		Criteria criteria = session.createCriteria(Period.class);
+		criteria.add(Restrictions.eq("periodCode", periodCode));
+		return (Period) criteria.uniqueResult();
+	}
 }
