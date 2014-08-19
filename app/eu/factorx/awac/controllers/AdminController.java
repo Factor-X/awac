d25d949da11758cf1e35779617ad403d5f012c28
Gaetan Paucot <gaetan.paucot@factorx.eu>
2014-08-19 13:41:59 +0200

    Results

--- app/eu/factorx/awac/controllers/AdminController.java
+++ app/eu/factorx/awac/controllers/AdminController.java
@@ -14,17 +14,11 @@ import eu.factorx.awac.util.data.importer.TranslationImporter;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
-import play.Logger;
-
 import play.db.jpa.JPA;
 import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
-import eu.factorx.awac.InitializationThread;
-import eu.factorx.awac.models.code.CodeList;
-import eu.factorx.awac.service.CodeLabelService;
-import eu.factorx.awac.util.data.importer.TranslationImporter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -88,8 +82,8 @@ public class AdminController extends Controller {
 		InMemoryData.translations = new HashMap<>();
 		InitializationThread.createInMemoryTranslations();
 
-		return(ok());
+		return (ok());
 	}
-	
+
 
 }
