221fdc28e2772c9c6e7359b53d98326ee1e5f41f
jcarton <jerome.carton@factorx.eu>
2014-08-19 12:47:54 +0200

    Fixed AcivitySource & ActivityType in BAD21

--- app/eu/factorx/awac/service/knowledge/activity/contributor/impl/BaseActivityDataAE_BAD21.java
+++ app/eu/factorx/awac/service/knowledge/activity/contributor/impl/BaseActivityDataAE_BAD21.java
@@ -5,10 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import eu.factorx.awac.models.code.type.ActivityCategoryCode;
-import eu.factorx.awac.models.code.type.ActivitySubCategoryCode;
-import eu.factorx.awac.models.code.type.BaseActivityDataCode;
-import eu.factorx.awac.models.code.type.QuestionCode;
+import eu.factorx.awac.models.code.type.*;
 import eu.factorx.awac.models.data.answer.QuestionAnswer;
 import eu.factorx.awac.models.data.answer.QuestionSetAnswer;
 import eu.factorx.awac.models.knowledge.Unit;
@@ -49,10 +46,8 @@ public class BaseActivityDataAE_BAD21 extends ActivityResultContributor {
 			baseActivityData.setSpecificPurpose("-atelier");
 			baseActivityData.setActivityCategory(ActivityCategoryCode.AC_7);
 			baseActivityData.setActivitySubCategory(ActivitySubCategoryCode.ASC_10);
-			// TODO: FLORIAN
-			// baseActivityData.setActivityType(ActivityTypeCode.EPURATION);
-			// TODO: FLORIAN
-			// baseActivityData.setActivitySource(ActivitySourceCode.EAU_USEE);
+			baseActivityData.setActivityType(ActivityTypeCode.AT_55);
+			baseActivityData.setActivitySource(ActivitySourceCode.AS_191);
 			baseActivityData.setActivityOwnership(null);
 			baseActivityData.setUnit(baseActivityDataUnit);
 			baseActivityData.setValue(toDouble(questionA183Answer, baseActivityDataUnit) / 2 * toDouble(questionA184Answer, baseActivityDataUnit) / 365);
