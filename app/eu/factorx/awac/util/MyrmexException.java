2522c570135d73cdc9dcf59ba37723d382394f00
jcarton <jerome.carton@factorx.eu>
2014-08-22 11:24:58 +0200

    Set "Rollback only" when a MyrmexException or a MyrmexRunitmeException is thrown in a transactional controller method

--- app/eu/factorx/awac/util/MyrmexException.java
+++ app/eu/factorx/awac/util/MyrmexException.java
@@ -1,5 +1,9 @@
 package eu.factorx.awac.util;
 
+import javax.persistence.EntityTransaction;
+
+import play.db.jpa.JPA;
+
 public class MyrmexException extends Exception {
 
 	private static final long serialVersionUID = 1L;
@@ -9,16 +13,26 @@ public class MyrmexException extends Exception {
 	public MyrmexException(String message) {
 		super(message);
 		toClientMessage = message;
+		setTransactionRollbackOnly();
 	}
 
 	public MyrmexException(BusinessErrorType message) {
 		super(message.name());
 		toClientMessage = message.name();
+		setTransactionRollbackOnly();
 	}
 
 	public MyrmexException(Throwable cause, String toClientMessage) {
 		super(cause);
 		this.toClientMessage = toClientMessage;
+		setTransactionRollbackOnly();
+	}
+
+	private void setTransactionRollbackOnly() {
+		EntityTransaction transaction = JPA.em().getTransaction();
+		if (transaction != null && transaction.isActive()) {			
+			transaction.setRollbackOnly();
+		}
 	}
 
 	public String getToClientMessage() {
