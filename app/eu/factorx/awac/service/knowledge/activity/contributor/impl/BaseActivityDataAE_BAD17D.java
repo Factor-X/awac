b15e224615c7f2c830d5bb74bfe27578072dd8c0
xaviermarichal <xavier.marichal@factorx.eu>
2014-08-22 12:30:02 +0200

    all BADs corrected until BAD18 included

--- app/eu/factorx/awac/service/knowledge/activity/contributor/impl/BaseActivityDataAE_BAD17D.java
+++ app/eu/factorx/awac/service/knowledge/activity/contributor/impl/BaseActivityDataAE_BAD17D.java
@@ -59,7 +59,7 @@ public class BaseActivityDataAE_BAD17D extends ActivityResultContributor {
 			baseActivityData.setActivitySource(ActivitySourceCode.AS_177);
 			baseActivityData.setActivityOwnership(false);
 			baseActivityData.setUnit(baseActivityDataUnit);
-			baseActivityData.setValue(toDouble(questionA145Answer, baseActivityDataUnit) * toDouble(questionA146Answer, baseActivityDataUnit) * toDouble(questionA150Answer, baseActivityDataUnit));
+            baseActivityData.setValue(toDouble(questionA145Answer, getUnitByCode(UnitCode.U5135)) * toDouble(questionA146Answer, getUnitByCode(UnitCode.U5106)) * toDouble(questionA150Answer));
 
 			res.add(baseActivityData);
 		}
