db39c93ad73e47fcf1f5e20da9de70419d7f2c00
gaston.hollands <gaston.hollands@factorx.eu>
2014-09-02 10:38:15 +0200

    - PDF SVG + PDF TEXT samples. - needs xalan 2.7.1 for working togheter.

--- app/eu/factorx/awac/controllers/PdfController.java
+++ app/eu/factorx/awac/controllers/PdfController.java
@@ -4,6 +4,7 @@ import eu.factorx.awac.common.actions.SecurityAnnotation;
 import eu.factorx.awac.util.email.messages.EmailMessage;
 import eu.factorx.awac.util.email.service.EmailService;
 import eu.factorx.awac.util.pdf.PDF;
+import eu.factorx.awac.util.pdf.SVG;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import play.Logger;
@@ -19,17 +20,28 @@ import eu.factorx.awac.views.html.pdf.*;
 //annotate as Spring Component
 @Component
 @Transactional(readOnly = true)
-@Security.Authenticated(SecuredController.class)
-@SecurityAnnotation(isAdmin = false, isSystemAdmin = false)
+//@Security.Authenticated(SecuredController.class)
+//@SecurityAnnotation(isAdmin = false, isSystemAdmin = false)
 public class PdfController extends Controller {
 
 	/**
-	 * Display list of accounts.
 	 * for PDF output
 	 *
 	 */
 	public static Result asPdf() {
 		return PDF.ok(document.render("this is my message"));
+	}
+
+	/**
+	 *
+	 * using SVG data
+	 *
+	 */
+
+	public static Result withSvg() {
+		SVG.toPDF();
+		//return PDF.ok(svg.render("this is a PDF with SVG"));
+		return (ok());
 	}
 
 }
---
+++ test/eu/factorx/awac/controllers/PDFTest.java
@@ -0,0 +1,80 @@
+package eu.factorx.awac.controllers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import eu.factorx.awac.dto.awac.get.LoginResultDTO;
+import eu.factorx.awac.dto.awac.post.EmailInvitationDTO;
+import eu.factorx.awac.dto.awac.post.EnterpriseAccountCreationDTO;
+import eu.factorx.awac.dto.awac.post.MunicipalityAccountCreationDTO;
+import eu.factorx.awac.dto.myrmex.get.PersonDTO;
+import eu.factorx.awac.dto.myrmex.post.ConnectionFormDTO;
+import eu.factorx.awac.models.AbstractBaseModelTest;
+import eu.factorx.awac.models.code.type.InterfaceTypeCode;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import play.libs.Json;
+import play.mvc.Result;
+import play.test.FakeRequest;
+
+import static org.junit.Assert.*;
+import static play.test.Helpers.*;
+
+@ContextConfiguration(locations = {"classpath:/components-test.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class PDFTest extends AbstractBaseModelTest {
+
+
+
+	@Test
+	public void _001_pdfGeneration() {
+
+		// ConnectionFormDTO
+		ConnectionFormDTO cfDto = new ConnectionFormDTO("user1", "password", InterfaceTypeCode.ENTERPRISE.getKey(), "");
+
+		// perform save
+		// Fake request
+		FakeRequest fr = new FakeRequest();
+		fr.withHeader("Content-type", "application/json");
+		fr.withSession(SecuredController.SESSION_IDENTIFIER_STORE, cfDto.getLogin());
+
+		// Call controller action
+		Result result = callAction(
+				routes.ref.PdfController.asPdf(),
+				fr
+		); // callAction
+
+		//analyse result
+		// expecting an HTTP 200 return code
+		assertEquals(200, status(result));
+	} // end of authenticateSuccess test
+
+	@Test
+	public void _001_pdfWithSvgGeneration() {
+
+		// ConnectionFormDTO
+		ConnectionFormDTO cfDto = new ConnectionFormDTO("user1", "password", InterfaceTypeCode.ENTERPRISE.getKey(), "");
+
+		// Fake request
+		FakeRequest fr = new FakeRequest();
+		fr.withHeader("Content-type", "application/json");
+		fr.withSession(SecuredController.SESSION_IDENTIFIER_STORE, cfDto.getLogin());
+
+		// Call controller action
+		Result result = callAction(
+				routes.ref.PdfController.withSvg(),
+				fr
+		); // callAction
+
+		//analyse result
+		// expecting an HTTP 200 return code
+		assertEquals(200, status(result));
+
+
+
+	} // end of authenticateSuccess test
+
+}
