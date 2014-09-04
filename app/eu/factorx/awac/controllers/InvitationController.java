f1d91bedb169d71de155b26bbc8da3c686ccaf5e
gaston.hollands <gaston.hollands@factorx.eu>
2014-09-04 14:29:51 +0200

    CELDL-101/102 - Scenario : Invitation des nouveaux utilisateurs.- US Répondre à l'invitation

--- app/eu/factorx/awac/controllers/InvitationController.java
+++ app/eu/factorx/awac/controllers/InvitationController.java
@@ -7,6 +7,7 @@ import eu.factorx.awac.dto.awac.get.LoginResultDTO;
 import eu.factorx.awac.dto.awac.post.EmailInvitationDTO;
 import eu.factorx.awac.dto.awac.post.EnterpriseAccountCreationDTO;
 import eu.factorx.awac.dto.awac.post.MunicipalityAccountCreationDTO;
+import eu.factorx.awac.dto.awac.post.RegisterInvitationDTO;
 import eu.factorx.awac.dto.myrmex.get.ExceptionsDTO;
 import eu.factorx.awac.dto.myrmex.get.PersonDTO;
 import eu.factorx.awac.models.account.Account;
@@ -31,10 +32,8 @@ import play.mvc.Security;
 
 
 @Transactional(readOnly = true)
-@Security.Authenticated(SecuredController.class)
-@SecurityAnnotation(isAdmin = true, isSystemAdmin = false)
 @org.springframework.stereotype.Controller
-public class InvitationController extends Controller {
+public class InvitationController extends AbstractController {
 
 
 	@Autowired
@@ -44,8 +43,8 @@ public class InvitationController extends Controller {
 	private InvitationService invitationService;
 
 
-
-	@Transactional(readOnly = false)
+	@Security.Authenticated(SecuredController.class)
+	@SecurityAnnotation(isAdmin = true, isSystemAdmin = false)
 	public Result launchInvitation () {
 
 		// get InvitationDTO from request
@@ -70,14 +69,27 @@ public class InvitationController extends Controller {
 		return ok(resultDto);
 	}
 
+	public Result registerInvitation () {
 
-	private static <T extends DTO> T extractDTOFromRequest(Class<T> DTOclass) {
-		T dto = DTO.getDTO(request().body().asJson(), DTOclass);
-		if (dto == null) {
-			throw new RuntimeException("The request content cannot be converted to a '" + DTOclass.getName() + "'.");
-		}
-		return dto;
+		Logger.info("request body:" + request().body().asJson());
+		// get InvitationDTO from request
+		RegisterInvitationDTO dto = extractDTOFromRequest(RegisterInvitationDTO.class);
+		Logger.info("Registering Invitation : " + dto.getEmail());
+		Logger.info("dump" + dto.toString());
+
+		//create InvitationResultDTO
+		InvitationResultDTO resultDto = new InvitationResultDTO ();
+		return ok(resultDto);
 	}
+
+//	@Override
+//	private static <T extends DTO> T extractDTOFromRequest(Class<T> DTOclass) {
+//		T dto = DTO.getDTO(request().body().asJson(), DTOclass);
+//		if (dto == null) {
+//			throw new RuntimeException("The request content cannot be converted to a '" + DTOclass.getName() + "'.");
+//		}
+//		return dto;
+//	}
 
 
 
