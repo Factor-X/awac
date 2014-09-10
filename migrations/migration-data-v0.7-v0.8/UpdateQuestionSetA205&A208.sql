4ac8cf8915e468781da40951e8295923c7b631f4
jcarton <jerome.carton@factorx.eu>
2014-09-10 17:21:18 +0200

    Added sql to move A208 QuestionSet from TAB6 to TAB4

--- migrations/migration-data-v0.7-v0.8/UpdateQuestionSetA205&A208.sql
+++ migrations/migration-data-v0.7-v0.8/UpdateQuestionSetA205&A208.sql
@@ -1,1 +1,2 @@
 update mm_form_questionset set form_id = 4 where questionset_id = (select id from question_set where code = 'A205');
+update mm_form_questionset set form_id = 4 where questionset_id = (select id from question_set where code = 'A208');
