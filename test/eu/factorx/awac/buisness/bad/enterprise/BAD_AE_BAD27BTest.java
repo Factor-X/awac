66204243fd1321abc92adff2193ec6cd46175d25
xaviermarichal <xavier.marichal@factorx.eu>
2014-09-17 12:37:22 +0200

    on the way to all BADs tested

--- test/eu/factorx/awac/buisness/bad/enterprise/BAD_AE_BAD27BTest.java
+++ test/eu/factorx/awac/buisness/bad/enterprise/BAD_AE_BAD27BTest.java
@@ -5,10 +5,9 @@ import eu.factorx.awac.controllers.SecuredController;
 import eu.factorx.awac.dto.awac.post.AnswerLineDTO;
 import eu.factorx.awac.dto.awac.post.QuestionAnswersDTO;
 import eu.factorx.awac.dto.myrmex.post.ConnectionFormDTO;
+import eu.factorx.awac.models.business.Scope;
 import eu.factorx.awac.models.business.Site;
-import eu.factorx.awac.models.code.type.InterfaceTypeCode;
-import eu.factorx.awac.models.code.type.QuestionCode;
-import eu.factorx.awac.models.code.type.UnitCode;
+import eu.factorx.awac.models.code.type.*;
 import eu.factorx.awac.models.data.answer.QuestionSetAnswer;
 import eu.factorx.awac.models.knowledge.Period;
 import eu.factorx.awac.models.reporting.BaseActivityData;
@@ -16,7 +15,8 @@ import eu.factorx.awac.service.PeriodService;
 import eu.factorx.awac.service.QuestionSetAnswerService;
 import eu.factorx.awac.service.ScopeService;
 import eu.factorx.awac.service.SiteService;
-import eu.factorx.awac.service.knowledge.activity.contributor.enterprise.BaseActivityDataAE_BAD27B;
+import eu.factorx.awac.service.knowledge.activity.contributor.enterprise.*;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import play.db.jpa.JPA;
@@ -35,7 +35,7 @@ import static play.test.Helpers.status;
  * Test for bad AE_BAD27B
 */
 @Component
-public class BAD_AE_BAD27BTest {
+public class BAD_AE_BAD27BTest{
 
     private static final Double ERROR_MARGE = 0.0001;
 
@@ -62,10 +62,9 @@ public class BAD_AE_BAD27BTest {
     /**
      * run test
      * need an id of a scope (site)
-     *
      * @param siteId
      */
-    public void test(long siteId) {
+    public void test(long siteId){
 
         Site site = siteService.findById(siteId);
         Period period = periodService.findById(PERIOD_ID);
@@ -82,12 +81,12 @@ public class BAD_AE_BAD27BTest {
         List<AnswerLineDTO> answerLineDTOList = new ArrayList<>();
 
         //add answers
-        answerLineDTOList.addAll(buildAnswerA210());
-        answerLineDTOList.addAll(buildAnswerA211());
-        answerLineDTOList.addAll(buildAnswerA212());
-        answerLineDTOList.addAll(buildAnswerA220());
-        answerLineDTOList.addAll(buildAnswerA221());
-
+                answerLineDTOList.addAll(buildAnswerA210());
+                answerLineDTOList.addAll(buildAnswerA211());
+                answerLineDTOList.addAll(buildAnswerA212());
+                answerLineDTOList.addAll(buildAnswerA220());
+                answerLineDTOList.addAll(buildAnswerA221());
+        
         questionAnswersDTO.setListAnswers(answerLineDTOList);
 
         //
@@ -101,12 +100,12 @@ public class BAD_AE_BAD27BTest {
 
         //connection
         ConnectionFormDTO cfDto = new ConnectionFormDTO(identifier, identifierPassword, InterfaceTypeCode.ENTERPRISE.getKey(), "");
-        saveFakeRequest.withSession(SecuredController.SESSION_IDENTIFIER_STORE, cfDto.getLogin());
+        saveFakeRequest.withSession(SecuredController.SESSION_IDENTIFIER_STORE,cfDto.getLogin());
 
         // Call controller action
         Result result = callAction(
-                eu.factorx.awac.controllers.routes.ref.AnswerController.save(),
-                saveFakeRequest
+            eu.factorx.awac.controllers.routes.ref.AnswerController.save(),
+            saveFakeRequest
         );
 
         // control result
@@ -124,14 +123,14 @@ public class BAD_AE_BAD27BTest {
         //control content
         //map mapResult
         Map<Double, Boolean> mapResult = new HashMap<>();
-        mapResult.put(2600.0, false);
-
+                mapResult.put(2600.0, false);
+        
         String valueGenerated = "";
 
-        for (BaseActivityData bad : bads) {
+        for(BaseActivityData bad : bads){
             valueGenerated += String.valueOf(bad.getValue()) + ",";
-            for (Map.Entry<Double, Boolean> entry : mapResult.entrySet()) {
-                if (around(entry.getKey(), bad.getValue())) {
+            for(Map.Entry<Double, Boolean> entry : mapResult.entrySet()){
+                if(around(entry.getKey(),bad.getValue())){
                     entry.setValue(true);
                 }
             }
@@ -139,112 +138,107 @@ public class BAD_AE_BAD27BTest {
 
         String valueNotFound = "";
 
-        for (Map.Entry<Double, Boolean> entry : mapResult.entrySet()) {
-            if (entry.getValue().equals(false)) {
-                valueNotFound += String.valueOf(entry.getKey()) + ", ";
+        for(Map.Entry<Double, Boolean> entry : mapResult.entrySet()){
+            if(entry.getValue().equals(false)){
+                valueNotFound+=String.valueOf(entry.getKey())+", ";
             }
         }
 
         //create errorMessage
-        assertTrue("Value expected but not found : " + valueNotFound + ". Value generated : " + valueGenerated, valueNotFound.length() == 0);
+        assertTrue("Value expected but not found : "+valueNotFound+". Value generated : "+valueGenerated,valueNotFound.length() == 0);
 
     }
 
-    /**
+        /**
      * build the AnswerLineDTO
      * question : A210
      */
-    private List<AnswerLineDTO> buildAnswerA210() {
+    private List<AnswerLineDTO> buildAnswerA210(){
 
         List<AnswerLineDTO> list = new ArrayList<>();
 
-        //add repetition
+                 //add repetition
         Map<String, Integer> mapRepetition1 = new HashMap<>();
-        mapRepetition1.put("A209", 1);
-        list.add(new AnswerLineDTO("A210", "acier", identifier, mapRepetition1));
-        //add repetition
+                mapRepetition1.put("A209",1);
+                list.add(new AnswerLineDTO("A210","acier", identifier, mapRepetition1 ));
+                //add repetition
         Map<String, Integer> mapRepetition2 = new HashMap<>();
-        mapRepetition2.put("A209", 2);
-        list.add(new AnswerLineDTO("A210", "informatique", identifier, mapRepetition2));
-
+                mapRepetition2.put("A209",2);
+                list.add(new AnswerLineDTO("A210","informatique", identifier, mapRepetition2 ));
+        
         return list;
     }
-
-    /**
+        /**
      * build the AnswerLineDTO
      * question : A211
      */
-    private List<AnswerLineDTO> buildAnswerA211() {
+    private List<AnswerLineDTO> buildAnswerA211(){
 
         List<AnswerLineDTO> list = new ArrayList<>();
 
-        //add repetition
+                 //add repetition
         Map<String, Integer> mapRepetition1 = new HashMap<>();
-        mapRepetition1.put("A209", 1);
-        list.add(new AnswerLineDTO("A211", "AT_61", identifier, mapRepetition1));
-        //add repetition
+                mapRepetition1.put("A209",1);
+                list.add(new AnswerLineDTO("A211","AT_61", identifier, mapRepetition1 ));
+                //add repetition
         Map<String, Integer> mapRepetition2 = new HashMap<>();
-        mapRepetition2.put("A209", 2);
-        list.add(new AnswerLineDTO("A211", "AT_68", identifier, mapRepetition2));
-
+                mapRepetition2.put("A209",2);
+                list.add(new AnswerLineDTO("A211","AT_68", identifier, mapRepetition2 ));
+        
         return list;
     }
-
-    /**
+        /**
      * build the AnswerLineDTO
      * question : A212
      */
-    private List<AnswerLineDTO> buildAnswerA212() {
+    private List<AnswerLineDTO> buildAnswerA212(){
 
         List<AnswerLineDTO> list = new ArrayList<>();
 
-        //add repetition
+                 //add repetition
         Map<String, Integer> mapRepetition1 = new HashMap<>();
-        mapRepetition1.put("A209", 1);
-        list.add(new AnswerLineDTO("A212", "AS_209", identifier, mapRepetition1));
-
+                mapRepetition1.put("A209",1);
+                list.add(new AnswerLineDTO("A212","AS_209", identifier, mapRepetition1 ));
+        
         return list;
     }
-
-    /**
+        /**
      * build the AnswerLineDTO
      * question : A220
      */
-    private List<AnswerLineDTO> buildAnswerA220() {
+    private List<AnswerLineDTO> buildAnswerA220(){
 
         List<AnswerLineDTO> list = new ArrayList<>();
 
-        //add repetition
+                 //add repetition
         Map<String, Integer> mapRepetition1 = new HashMap<>();
-        mapRepetition1.put("A209", 1);
-        list.add(new AnswerLineDTO("A220", 0.65, identifier, mapRepetition1));
-
+                mapRepetition1.put("A209",1);
+                list.add(new AnswerLineDTO("A220",0.65, identifier, mapRepetition1 ));
+        
         return list;
     }
-
-    /**
+        /**
      * build the AnswerLineDTO
      * question : A221
      */
-    private List<AnswerLineDTO> buildAnswerA221() {
+    private List<AnswerLineDTO> buildAnswerA221(){
 
         List<AnswerLineDTO> list = new ArrayList<>();
 
-        //add repetition
+                 //add repetition
         Map<String, Integer> mapRepetition1 = new HashMap<>();
-        mapRepetition1.put("A209", 1);
-        list.add(new AnswerLineDTO("A221", 4000.0, identifier, mapRepetition1, UnitCode.U5135.getKey()));
-
+                mapRepetition1.put("A209",1);
+                list.add(new AnswerLineDTO("A221",4000.0, identifier, mapRepetition1  , UnitCode.U5135.getKey()  ));
+        
         return list;
     }
-
+    
 
     /**
      * control all except value
-     *
      * @param bad
      */
-    private void controlGlobalBad(BaseActivityData bad) {
+    private void controlGlobalBad(BaseActivityData bad){
         /*
         assertTrue("BaseActivityDataCode error. Expected : ActivityCategoryCode.AC_8, founded : "+bad.getKey(),bad.getKey().equals(BaseActivityDataCode.ActivityCategoryCode.AC_8));
         assertTrue("Rank error : Expected : 1, founded : "+bad.getRank(),bad.getRank().equals(1));
@@ -258,10 +252,10 @@ public class BAD_AE_BAD27BTest {
         */
     }
 
-    private boolean around(Double value1, Double value2) {
-        if (value1 >= value2 * (1 - ERROR_MARGE) && value1 <= value2 * (1 + ERROR_MARGE)) {
+    private boolean around(Double value1,Double value2){
+        if(value1>=value2*(1-ERROR_MARGE) && value1 <= value2*(1+ERROR_MARGE)){
             return true;
         }
-        return false;
+       return false;
     }
 }
