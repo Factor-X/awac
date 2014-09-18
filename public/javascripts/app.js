de74e3d72419ce7042cd98ec01f12b64e87c2f6c
Gaetan Paucot <gaetan.paucot@factorx.eu>
2014-09-18 12:20:40 +0200

    before merge

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2951,7 +2951,10 @@ angular.module('app.directives').directive('mmSizeValidator', function(){
 });angular.module('app.controllers').controller("ResultsCtrl", function($scope, $window, downloadService, displayFormMenu, modalService) {
   $scope.displayFormMenu = displayFormMenu;
   modalService.show(modalService.LOADING);
-  downloadService.getJson("/awac/result/getReport/" + $scope.$parent.periodKey + "/" + $scope.$parent.scopeId, function(result) {
+  downloadService.postJson('/awac/result/getReport', {
+    periodKey: $scope.$parent.periodKey,
+    scopesIds: [$scope.$parent.scopeId]
+  }, function(result) {
     modalService.close(modalService.LOADING);
     if (result.success) {
       return $scope.o = result.data;
@@ -2959,13 +2962,6 @@ angular.module('app.directives').directive('mmSizeValidator', function(){
       ;
     }
   });
-  $scope.charts = {
-    histogramUrl: "/awac/result/getHistogram/" + $scope.$parent.periodKey + "/" + $scope.$parent.scopeId,
-    webUrl: "/awac/result/getWeb/" + $scope.$parent.periodKey + "/" + $scope.$parent.scopeId,
-    donutUrl1: "/awac/result/getDonut/" + $scope.$parent.periodKey + "/" + $scope.$parent.scopeId + "/1",
-    donutUrl2: "/awac/result/getDonut/" + $scope.$parent.periodKey + "/" + $scope.$parent.scopeId + "/2",
-    donutUrl3: "/awac/result/getDonut/" + $scope.$parent.periodKey + "/" + $scope.$parent.scopeId + "/3"
-  };
   $scope.current_tab = 1;
   $scope.downloadAsXls = function() {
     return $window.open('/awac/result/getReportAsXls/' + $scope.$parent.periodKey + "/" + $scope.$parent.scopeId, "Downloading report file...", null);
