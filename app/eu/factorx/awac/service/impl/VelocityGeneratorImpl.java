9b195c5311bb98295d17c02c26a7e0cbe333e8bd
gaston.hollands <gaston.hollands@factorx.eu>
2014-09-23 02:52:27 +0200

    Velovity on Heroku tests

--- app/eu/factorx/awac/service/impl/VelocityGeneratorImpl.java
+++ app/eu/factorx/awac/service/impl/VelocityGeneratorImpl.java
@@ -14,6 +14,9 @@ package eu.factorx.awac.service.impl;
 
 import eu.factorx.awac.service.VelocityGeneratorService;
 import jp.furyu.play.velocity.mvc.package$;
+import org.apache.commons.io.IOUtils;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
 import org.springframework.stereotype.Component;
 import play.Logger;
 import play.api.Play;
@@ -23,7 +26,8 @@ import play.mvc.Result;
 import play.mvc.Results;
 
 
-import java.io.InputStream;
+import java.io.*;
+import java.util.Iterator;
 import java.util.Map;
 
 @Component
@@ -40,55 +44,48 @@ public class VelocityGeneratorImpl implements VelocityGeneratorService {
 
 	public String generate(String templateName, Map values) {
 
+		InputStream in = Play.classloader(play.api.Play.current()).getResourceAsStream(ROOT+templateName);
+		String velocityTemplate= new String("");
 
-		String path = Play.current().path().getPath();
-		Logger.info("Play path :" + path);
-
-		String absolutePath = Play.current().path().getAbsolutePath();
-		Logger.info("Play absolute path :" + absolutePath);
-
-		try	{
-			String canonicalPath = Play.current().path().getCanonicalPath();
-			Logger.info("Play canocical path :" + canonicalPath);
+		try {
+			velocityTemplate = IOUtils.toString(in, "UTF8");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+		Logger.info("Velocity Template Content : " + velocityTemplate);
 
-		//Play.classloader(play.api.Play.current()).getResource("any_file");
-
-		Logger.info("Play before input stream...");
-		InputStream in = Play.classloader(play.api.Play.current()).getResourceAsStream(ROOT+templateName);
-		Logger.info("Play before serverPath...");
-
-		String serverPath = "";
-
-		if (play.Play.isDev()) {
-			serverPath = Play.classloader(play.api.Play.current()).getResource(ROOT + templateName).toString().replace("file:", "");
-		} else {
-			serverPath = Play.classloader(play.api.Play.current()).getResource(ROOT + templateName).toString().replace("file:", "");
-			serverPath = serverPath.replace("jar:", "");
-			serverPath = serverPath.replace("!", "");
-		}
-
-		Logger.info("Play original server Path :" + serverPath);
-		//String serverPath = Play.classloader(play.api.Play.current()).getResource("public/vm/launchInvitation.vm").getPath();
-		serverPath = serverPath.replace(Play.current().path().getAbsolutePath(),"");
-
-		Logger.info("Play input stream :" + in);
-		Logger.info("Play replaced server Path :" + serverPath);
+//		String serverPath = "";
+//
+//		if (play.Play.isDev()) {
+//			serverPath = Play.classloader(play.api.Play.current()).getResource(ROOT + templateName).toString().replace("file:", "");
+//		} else {
+//			serverPath = Play.classloader(play.api.Play.current()).getResource(ROOT + templateName).toString().replace("file:", "");
+//			serverPath = serverPath.replace("jar:", "");
+//			serverPath = serverPath.replace("!", "");
+//		}
+//
+//		Logger.info("Play original server Path :" + serverPath);
+//		//String serverPath = Play.classloader(play.api.Play.current()).getResource("public/vm/launchInvitation.vm").getPath();
+//		serverPath = serverPath.replace(Play.current().path().getAbsolutePath(),"");
+//
+//		Logger.info("Play input stream :" + in);
+//		Logger.info("Play replaced server Path :" + serverPath);
 
 
-		StringBuffer html = new StringBuffer();
-		//html.append(package$.MODULE$.VM(ROOT + templateName, Scala.asScala(values), "utf-8"));
-		if (play.Play.isDev()) {
-			html.append(package$.MODULE$.VM(serverPath, Scala.asScala(values), "utf-8"));
-		} else {
-			html.append(package$.MODULE$.VM(ROOT_PROD + templateName , Scala.asScala(values), "utf-8"));
-		}
+//		Logger.info("velocity return: " + velocityRender(theString,values));
+
+//		StringBuffer html = new StringBuffer();
+//		//html.append(package$.MODULE$.VM(ROOT + templateName, Scala.asScala(values), "utf-8"));
+//		if (play.Play.isDev()) {
+//			html.append(package$.MODULE$.VM(serverPath, Scala.asScala(values), "utf-8"));
+//		} else {
+//			html.append(package$.MODULE$.VM(ROOT_PROD + templateName , Scala.asScala(values), "utf-8"));
+//		}
 
 		//Logger.info("HTML generated:" + html.toString());
-		return (html.toString());
+
+		return (velocityRender(velocityTemplate,values));
 	}
 
 	private String getCurrentFullMethodName() {
@@ -101,6 +98,42 @@ public class VelocityGeneratorImpl implements VelocityGeneratorService {
 
 	public String getTemplateNameByMethodName() {
 		return Thread.currentThread().getStackTrace()[2].getMethodName() + ".vm";
+	}
+
+	private String velocityRender (String template,Map values) {
+
+		StringBuffer Html = new StringBuffer();
+
+		VelocityEngine ve = new VelocityEngine();
+		ve.init();
+
+		VelocityContext context = new VelocityContext();
+
+		Iterator it = values.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry entry = (Map.Entry)it.next();
+			context.put(entry.getKey().toString(), entry.getValue());
+		}
+
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		OutputStreamWriter html = new OutputStreamWriter(output);
+		//OutputStream html = new OutputStream(output);
+
+		StringWriter result = new StringWriter();
+		try {
+			if (!ve.evaluate(context, result, "report", template)) {
+				Logger.info("Evaluation error");
+			} else {
+				Logger.info("Evaluation success");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Logger.info("Render result:" + result.toString());
+		return (result.toString());
+
 	}
 
 } // end of class
