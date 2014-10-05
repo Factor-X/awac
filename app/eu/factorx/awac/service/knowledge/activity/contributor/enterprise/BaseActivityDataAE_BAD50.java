52298cfc9908253ff75cc16a0cc76e7d56985910
xaviermarichal <xavier.marichal@factorx.eu>
2014-10-05 19:57:40 +0200

    end of CELDL-204 resolution

--- app/eu/factorx/awac/service/knowledge/activity/contributor/enterprise/BaseActivityDataAE_BAD50.java
+++ app/eu/factorx/awac/service/knowledge/activity/contributor/enterprise/BaseActivityDataAE_BAD50.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Component;
 * Name of the BAD : Site: combustion directe
  */
 @Component
-public class BaseActivityDataAE_BAD1 extends ActivityResultContributor {
+public class BaseActivityDataAE_BAD50 extends ActivityResultContributor {
 
     @Override
     public List<BaseActivityData> getBaseActivityData(Map<QuestionCode, List<QuestionSetAnswer>> questionSetAnswers) {
@@ -30,8 +30,8 @@ public class BaseActivityDataAE_BAD1 extends ActivityResultContributor {
         //  - b) conversion
 
         // Get Target Unit (GJ in this case)
-        // Allow finding unit by a UnitCode (unit symbole = GJ )
-        Unit baseActivityDataUnit = getUnitByCode(UnitCode.U5321);
+        // Allow finding unit by a UnitCode (unit symbole = l )
+        Unit baseActivityDataUnit = getUnitByCode(UnitCode.U5126);
 
 
         //2. build BAD
@@ -40,25 +40,25 @@ public class BaseActivityDataAE_BAD1 extends ActivityResultContributor {
         //2.1 loop for each answer for each loop for each parents of one or more question needed by the BAD
         
             
-                            List<QuestionSetAnswer> questionSetAnswersA15 = questionSetAnswers.get(QuestionCode.A15);
+                            List<QuestionSetAnswer> questionSetAnswersA1000 = questionSetAnswers.get(QuestionCode.A1000);
                 //2.2 control if the list if different than null
-                if (questionSetAnswersA15 == null) {
+                if (questionSetAnswersA1000 == null) {
                     return res;
                 }
             
             //loop ($repetition.mainQuestionSetDescription)
-                            for (QuestionSetAnswer questionSetAnswer1 : questionSetAnswersA15) {
+                            for (QuestionSetAnswer questionSetAnswer1 : questionSetAnswersA1000) {
              
                             //create a map for each repetition level
                 Map<QuestionCode, QuestionAnswer> answersByCode1 = byQuestionCode(questionSetAnswer1.getQuestionAnswers());
 
                 //load question for this level
-                                    QuestionAnswer questionA16Answer = answersByCode1.get(QuestionCode.A16);
-                                    QuestionAnswer questionA17Answer = answersByCode1.get(QuestionCode.A17);
+                                    QuestionAnswer questionA1001Answer = answersByCode1.get(QuestionCode.A1001);
+                                    QuestionAnswer questionA1002Answer = answersByCode1.get(QuestionCode.A1002);
                 
                     //control them
-                    if (                            questionA16Answer == null  ||
-                                                                                questionA17Answer == null                                                 ) {
+                    if (                            questionA1001Answer == null  ||
+                                                                                questionA1002Answer == null                                                 ) {
                        continue;
                     }
                     
@@ -66,18 +66,18 @@ public class BaseActivityDataAE_BAD1 extends ActivityResultContributor {
             //build the bad
             BaseActivityData baseActivityData = new BaseActivityData();
 
-            baseActivityData.setKey(BaseActivityDataCode.AE_BAD1);
+            baseActivityData.setKey(BaseActivityDataCode.AE_BAD50);
             baseActivityData.setRank(1);
             baseActivityData.setSpecificPurpose(null);
             baseActivityData.setActivityCategory(ActivityCategoryCode.AC_1);
             baseActivityData.setActivitySubCategory(ActivitySubCategoryCode.ASC_1);
             baseActivityData.setActivityType(ActivityTypeCode.AT_1);
 
-            baseActivityData.setActivitySource(toActivitySourceCode(questionA16Answer));
+            baseActivityData.setActivitySource(toActivitySourceCode(questionA1001Answer));
 
             baseActivityData.setActivityOwnership(true);
             baseActivityData.setUnit(baseActivityDataUnit);
-            baseActivityData.setValue(toDouble(questionA17Answer, getUnitByCode(UnitCode.U5321)));
+            baseActivityData.setValue(toDouble(questionA1002Answer, getUnitByCode(UnitCode.U5126)));
             res.add(baseActivityData);
 
             
--- app/eu/factorx/awac/service/knowledge/activity/contributor/enterprise/BaseActivityDataAE_BAD51.java
+++ app/eu/factorx/awac/service/knowledge/activity/contributor/enterprise/BaseActivityDataAE_BAD51.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Component;
 * Name of the BAD : Site: combustion directe
  */
 @Component
-public class BaseActivityDataAE_BAD1 extends ActivityResultContributor {
+public class BaseActivityDataAE_BAD51 extends ActivityResultContributor {
 
     @Override
     public List<BaseActivityData> getBaseActivityData(Map<QuestionCode, List<QuestionSetAnswer>> questionSetAnswers) {
@@ -30,8 +30,8 @@ public class BaseActivityDataAE_BAD1 extends ActivityResultContributor {
         //  - b) conversion
 
         // Get Target Unit (GJ in this case)
-        // Allow finding unit by a UnitCode (unit symbole = GJ )
-        Unit baseActivityDataUnit = getUnitByCode(UnitCode.U5321);
+        // Allow finding unit by a UnitCode (unit symbole = t )
+        Unit baseActivityDataUnit = getUnitByCode(UnitCode.U5135);
 
 
         //2. build BAD
@@ -40,25 +40,25 @@ public class BaseActivityDataAE_BAD1 extends ActivityResultContributor {
         //2.1 loop for each answer for each loop for each parents of one or more question needed by the BAD
         
             
-                            List<QuestionSetAnswer> questionSetAnswersA15 = questionSetAnswers.get(QuestionCode.A15);
+                            List<QuestionSetAnswer> questionSetAnswersA1003 = questionSetAnswers.get(QuestionCode.A1003);
                 //2.2 control if the list if different than null
-                if (questionSetAnswersA15 == null) {
+                if (questionSetAnswersA1003 == null) {
                     return res;
                 }
             
             //loop ($repetition.mainQuestionSetDescription)
-                            for (QuestionSetAnswer questionSetAnswer1 : questionSetAnswersA15) {
+                            for (QuestionSetAnswer questionSetAnswer1 : questionSetAnswersA1003) {
              
                             //create a map for each repetition level
                 Map<QuestionCode, QuestionAnswer> answersByCode1 = byQuestionCode(questionSetAnswer1.getQuestionAnswers());
 
                 //load question for this level
-                                    QuestionAnswer questionA16Answer = answersByCode1.get(QuestionCode.A16);
-                                    QuestionAnswer questionA17Answer = answersByCode1.get(QuestionCode.A17);
+                                    QuestionAnswer questionA1004Answer = answersByCode1.get(QuestionCode.A1004);
+                                    QuestionAnswer questionA1005Answer = answersByCode1.get(QuestionCode.A1005);
                 
                     //control them
-                    if (                            questionA16Answer == null  ||
-                                                                                questionA17Answer == null                                                 ) {
+                    if (                            questionA1004Answer == null  ||
+                                                                                questionA1005Answer == null                                                 ) {
                        continue;
                     }
                     
@@ -66,18 +66,18 @@ public class BaseActivityDataAE_BAD1 extends ActivityResultContributor {
             //build the bad
             BaseActivityData baseActivityData = new BaseActivityData();
 
-            baseActivityData.setKey(BaseActivityDataCode.AE_BAD1);
+            baseActivityData.setKey(BaseActivityDataCode.AE_BAD51);
             baseActivityData.setRank(1);
             baseActivityData.setSpecificPurpose(null);
             baseActivityData.setActivityCategory(ActivityCategoryCode.AC_1);
             baseActivityData.setActivitySubCategory(ActivitySubCategoryCode.ASC_1);
             baseActivityData.setActivityType(ActivityTypeCode.AT_1);
 
-            baseActivityData.setActivitySource(toActivitySourceCode(questionA16Answer));
+            baseActivityData.setActivitySource(toActivitySourceCode(questionA1004Answer));
 
             baseActivityData.setActivityOwnership(true);
             baseActivityData.setUnit(baseActivityDataUnit);
-            baseActivityData.setValue(toDouble(questionA17Answer, getUnitByCode(UnitCode.U5321)));
+            baseActivityData.setValue(toDouble(questionA1005Answer, getUnitByCode(UnitCode.U5135)));
             res.add(baseActivityData);
 
             
