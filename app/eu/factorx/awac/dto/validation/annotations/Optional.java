f26f10d7846a2908434386ad1733d8937a324e1b
jcarton <jerome.carton@factorx.eu>
2014-10-08 15:18:39 +0200

    merge corrections made on deploy-v0.9 branch

---
+++ app/eu/factorx/awac/dto/validation/annotations/Optional.java
@@ -0,0 +1,9 @@
+package eu.factorx.awac.dto.validation.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+
+@java.lang.annotation.Target({ElementType.FIELD})
+@java.lang.annotation.Retention(RetentionPolicy.RUNTIME)
+public @interface Optional {
+}
