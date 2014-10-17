25c1f2d8a6bccd4bca32ede11be46020355094a5
Gaetan Paucot <gaetan.paucot@factorx.eu>
2014-10-17 09:30:28 +0200

    xls

---
+++ app/eu/factorx/awac/util/math/Vector2I.java
@@ -0,0 +1,32 @@
+package eu.factorx.awac.util.math;
+
+public class Vector2I {
+
+	private int x, y;
+
+	public Vector2I(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + x + "," + y + ")";
+	}
+}
