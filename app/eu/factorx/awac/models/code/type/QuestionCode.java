d8d2248d9fc40d0cdcdf20f251f8eee1823e0cda
gaston.hollands <gaston.hollands@factorx.eu>
2014-10-20 09:39:25 +0200

    Merge branch 'master' of /home/gaston/devel/factorx/awac with conflicts.

--- app/eu/factorx/awac/models/code/type/QuestionCode.java
+++ app/eu/factorx/awac/models/code/type/QuestionCode.java
@@ -13,7 +13,7 @@ public class QuestionCode extends Code {
 	/* BEGIN GENERATED QUESTION_CODES ENTERPRISE */
 
 
-/* Generated the 2014-10-18 13:11:43 +0200 */
+/* Generated the 2014-10-17 10:39:50 +0200 */
 
 public static final QuestionCode A1 = new QuestionCode("A1");
 public static final QuestionCode A2 = new QuestionCode("A2");
@@ -404,7 +404,7 @@ public static final QuestionCode A8000 = new QuestionCode("A8000");
 	/* BEGIN GENERATED QUESTION_CODES MUNICIPALITY */
 
 
-/* Generated the 2014-10-18 13:11:48 +0200 */
+/* Generated the 2014-10-17 10:39:52 +0200 */
 
 public static final QuestionCode AC1 = new QuestionCode("AC1");
 public static final QuestionCode AC2 = new QuestionCode("AC2");
