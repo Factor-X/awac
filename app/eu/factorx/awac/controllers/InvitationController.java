b29dd3e664b583f603ffba872c391f97f291b7f3
gaston.hollands <gaston.hollands@factorx.eu>
2014-10-21 14:08:48 +0200

    Correct test errors

--- app/eu/factorx/awac/controllers/InvitationController.java
+++ app/eu/factorx/awac/controllers/InvitationController.java
@@ -146,12 +146,12 @@ public class InvitationController extends AbstractController {
 
         // ONLY FOR municipality : assign the new user of the site
         //if (securedController.getCurrentUser().getOrganization().getInterfaceCode().equals(InterfaceTypeCode.MUNICIPALITY)) {
-		if (invitation.getOrganization().getInterfaceCode().equals(InterfaceTypeCode.MUNICIPALITY)) {
-            for (Site site : invitation.getOrganization().getSites()) {
-                AccountSiteAssociation accountSiteAssociation = new AccountSiteAssociation(site, account);
-                accountSiteAssociationService.saveOrUpdate(accountSiteAssociation);
-            }
-        }
+//		if (invitation.getOrganization().getInterfaceCode().equals(InterfaceTypeCode.MUNICIPALITY)) {
+//            for (Site site : invitation.getOrganization().getSites()) {
+//                AccountSiteAssociation accountSiteAssociation = new AccountSiteAssociation(site, account);
+//                accountSiteAssociationService.saveOrUpdate(accountSiteAssociation);
+//            }
+//        }
 
         // delete invitation
         invitationService.remove(invitation);
