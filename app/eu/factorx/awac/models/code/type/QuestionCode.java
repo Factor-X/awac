ca93671de2675293e75ee0c8273609c86db67a83
gaston.hollands <gaston.hollands@factorx.eu>
2014-10-22 16:46:57 +0200

    do not push

--- app/eu/factorx/awac/models/code/type/QuestionCode.java
+++ app/eu/factorx/awac/models/code/type/QuestionCode.java
@@ -13,7 +13,7 @@ public class QuestionCode extends Code {
 	/* BEGIN GENERATED QUESTION_CODES ENTERPRISE */
 
 
-/* Generated the 2014-10-22 07:52:58 +0200 */
+/* Generated the 2014-10-22 14:18:09 +0200 */
 
 public static final QuestionCode A1 = new QuestionCode("A1");
 public static final QuestionCode A2 = new QuestionCode("A2");
@@ -399,7 +399,7 @@ public static final QuestionCode A8000 = new QuestionCode("A8000");
 	/* BEGIN GENERATED QUESTION_CODES MUNICIPALITY */
 
 
-/* Generated the 2014-10-22 07:53:01 +0200 */
+/* Generated the 2014-10-22 14:18:11 +0200 */
 
 public static final QuestionCode AC1 = new QuestionCode("AC1");
 public static final QuestionCode AC2 = new QuestionCode("AC2");
