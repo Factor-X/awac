b1f75b88dfcbd0d8662ca2f5d65f56fe6f2a0575
jcarton <jerome.carton@factorx.eu>
2014-11-05 13:35:13 +0100

    Reducing Actions (CELDL-77) - Advices: implemented computing of GHG benefit

--- app/eu/factorx/awac/service/ReportResultService.java
+++ app/eu/factorx/awac/service/ReportResultService.java
@@ -5,19 +5,20 @@ import java.util.List;
 import eu.factorx.awac.models.business.Scope;
 import eu.factorx.awac.models.forms.AwacCalculator;
 import eu.factorx.awac.models.knowledge.Period;
+import eu.factorx.awac.models.reporting.BaseActivityResult;
 import eu.factorx.awac.models.reporting.ReportResult;
-import eu.factorx.awac.service.impl.reporting.MergedReportResultCollectionAggregation;
-import eu.factorx.awac.service.impl.reporting.ReportResultAggregation;
-import eu.factorx.awac.service.impl.reporting.ReportResultCollection;
-import eu.factorx.awac.service.impl.reporting.ReportResultCollectionAggregation;
+import eu.factorx.awac.service.impl.reporting.*;
 
 public interface ReportResultService {
 
 	ReportResultCollection getReportResults(AwacCalculator awacCalculator, List<Scope> scopes, Period period);
+
+	List<BaseActivityResult> getBaseActivityResults(AwacCalculator awacCalculator, List<Scope> scopes, Period period, List<ReportLogEntry> logEntries);
 
 	ReportResultCollectionAggregation aggregate(ReportResultCollection reportResultCollection);
 
 	ReportResultAggregation aggregate(ReportResult reportResult);
 
 	MergedReportResultCollectionAggregation mergeAsComparision(ReportResultCollectionAggregation a1, ReportResultCollectionAggregation a2);
+
 }
