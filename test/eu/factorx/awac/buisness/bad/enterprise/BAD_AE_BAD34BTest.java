04789835277942f9504c1bbcc3dbb0e9418b3466
xaviermarichal <xavier.marichal@factorx.eu>
2014-12-01 12:43:19 +0100

    modified BADs because of unit changes for factors

---
+++ test/eu/factorx/awac/buisness/bad/enterprise/BAD_AE_BAD34BTest.java
@@ -0,0 +1,214 @@
+package eu.factorx.awac.buisness.bad.enterprise;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import eu.factorx.awac.controllers.SecuredController;
+import eu.factorx.awac.dto.awac.post.AnswerLineDTO;
+import eu.factorx.awac.dto.awac.post.QuestionAnswersDTO;
+import eu.factorx.awac.dto.myrmex.post.ConnectionFormDTO;
+import eu.factorx.awac.models.business.Scope;
+import eu.factorx.awac.models.business.Site;
+import eu.factorx.awac.models.code.type.*;
+import eu.factorx.awac.models.data.answer.QuestionSetAnswer;
+import eu.factorx.awac.models.knowledge.Period;
+import eu.factorx.awac.models.reporting.BaseActivityData;
+import eu.factorx.awac.service.PeriodService;
+import eu.factorx.awac.service.QuestionSetAnswerService;
+import eu.factorx.awac.service.ScopeService;
+import eu.factorx.awac.service.SiteService;
+import eu.factorx.awac.service.knowledge.activity.contributor.enterprise.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import play.db.jpa.JPA;
+import play.libs.Json;
+import play.mvc.Result;
+import play.test.FakeRequest;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static play.test.Helpers.callAction;
+import static play.test.Helpers.status;
+
+/*
+ * Test for bad AE_BAD34B
+*/
+@Component
+public class BAD_AE_BAD34BTest{
+
+    private static final Double ERROR_MARGE = 0.0001;
+
+    @Autowired
+    private QuestionSetAnswerService questionSetAnswerService;
+
+    @Autowired
+    private SiteService siteService;
+
+    @Autowired
+    private ScopeService scopeService;
+
+    @Autowired
+    private PeriodService periodService;
+
+    @Autowired
+    private BaseActivityDataAE_BAD34B baseActivityDataAE_BAD34B;
+
+    private final static Long FORM_ID = 2L;
+    private final static Long PERIOD_ID = 1L;
+    private String identifier = "user1";
+    private String identifierPassword = "password";
+
+    /**
+     * run test
+     * need an id of a scope (site)
+     * @param siteId
+     */
+    public void test(long siteId){
+
+        Site site = siteService.findById(siteId);
+        Period period = periodService.findById(PERIOD_ID);
+
+        //
+        // 1) build data
+        //
+        QuestionAnswersDTO questionAnswersDTO = new QuestionAnswersDTO();
+        questionAnswersDTO.setFormId(FORM_ID);
+        questionAnswersDTO.setPeriodId(PERIOD_ID);
+        questionAnswersDTO.setScopeId(site.getId());
+        questionAnswersDTO.setLastUpdateDate(new Date().toString());
+
+        List<AnswerLineDTO> answerLineDTOList = new ArrayList<>();
+
+        //add answers
+                answerLineDTOList.addAll(buildAnswerA245());
+                answerLineDTOList.addAll(buildAnswerA287());
+        
+        questionAnswersDTO.setListAnswers(answerLineDTOList);
+
+        //
+        // 2) send request
+        //
+        //Json node
+        JsonNode node = Json.toJson(questionAnswersDTO);
+        FakeRequest saveFakeRequest = new FakeRequest();
+        saveFakeRequest.withHeader("Content-type", "application/json");
+        saveFakeRequest.withJsonBody(node);
+
+        //connection
+        ConnectionFormDTO cfDto = new ConnectionFormDTO(identifier, identifierPassword, InterfaceTypeCode.ENTERPRISE.getKey(), "");
+        saveFakeRequest.withSession(SecuredController.SESSION_IDENTIFIER_STORE,cfDto.getLogin());
+
+        // Call controller action
+        Result result = callAction(
+            eu.factorx.awac.controllers.routes.ref.AnswerController.save(),
+            saveFakeRequest
+        );
+
+        // control result
+        assertEquals(200, status(result));
+
+        //
+        // 3) control BAD
+        //
+        //TODO temporary
+        JPA.em().clear();
+        JPA.em().flush();
+        Map<QuestionCode, List<QuestionSetAnswer>> questionSetAnswers = questionSetAnswerService.getAllQuestionSetAnswers(site, period);
+        List<BaseActivityData> bads = baseActivityDataAE_BAD34B.getBaseActivityData(questionSetAnswers);
+
+        //control content
+        //map mapResult
+        Map<Double, Boolean> mapResult = new HashMap<>();
+                mapResult.put(2500.0, false);
+                mapResult.put(6000.0, false);
+        
+        String valueGenerated = "";
+
+        for(BaseActivityData bad : bads){
+            valueGenerated += String.valueOf(bad.getValue()) + ",";
+            for(Map.Entry<Double, Boolean> entry : mapResult.entrySet()){
+                if(around(entry.getKey(),bad.getValue())){
+                    entry.setValue(true);
+                }
+            }
+        }
+
+        String valueNotFound = "";
+
+        for(Map.Entry<Double, Boolean> entry : mapResult.entrySet()){
+            if(entry.getValue().equals(false)){
+                valueNotFound+=String.valueOf(entry.getKey())+", ";
+            }
+        }
+
+        //create errorMessage
+        assertTrue("Value expected but not found : "+valueNotFound+". Value generated : "+valueGenerated,valueNotFound.length() == 0);
+
+    }
+
+        /**
+     * build the AnswerLineDTO
+     * question : A245
+     */
+    private List<AnswerLineDTO> buildAnswerA245(){
+
+        List<AnswerLineDTO> list = new ArrayList<>();
+
+                 //add repetition
+        Map<String, Integer> mapRepetition1 = new HashMap<>();
+                mapRepetition1.put("A244",1);
+                list.add(new AnswerLineDTO("A245","produitA",  mapRepetition1 ));
+                //add repetition
+        Map<String, Integer> mapRepetition2 = new HashMap<>();
+                mapRepetition2.put("A244",2);
+                list.add(new AnswerLineDTO("A245","produitB",  mapRepetition2 ));
+        
+        return list;
+    }
+        /**
+     * build the AnswerLineDTO
+     * question : A287
+     */
+    private List<AnswerLineDTO> buildAnswerA287(){
+
+        List<AnswerLineDTO> list = new ArrayList<>();
+
+                 //add repetition
+        Map<String, Integer> mapRepetition1 = new HashMap<>();
+                mapRepetition1.put("A244",1);
+                list.add(new AnswerLineDTO("A287",2500.0,  mapRepetition1  , UnitCode.U5156.getKey()  ));
+                //add repetition
+        Map<String, Integer> mapRepetition2 = new HashMap<>();
+                mapRepetition2.put("A244",2);
+                list.add(new AnswerLineDTO("A287",6000.0,  mapRepetition2  , UnitCode.U5156.getKey()  ));
+        
+        return list;
+    }
+    
+
+    /**
+     * control all except value
+     * @param bad
+     */
+    private void controlGlobalBad(BaseActivityData bad){
+        /*
+        assertTrue("BaseActivityDataCode error. Expected : ActivityCategoryCode.AC_11, founded : "+bad.getKey(),bad.getKey().equals(BaseActivityDataCode.ActivityCategoryCode.AC_11));
+        assertTrue("Rank error : Expected : 0, founded : "+bad.getRank(),bad.getRank().equals(0));
+        assertTrue("SpecificPurpose error : Expected : {}, founded : "+bad.getSpecificPurpose(),bad.getSpecificPurpose() == null);
+        assertTrue("ActivityCategory error : Expected : {}, founded : "+bad.getActivityCategory(),bad.getActivityCategory().equals(ActivityCategoryCode.AC_1));
+        assertTrue("ActivitySubCategory error : Expected : {}, founded : "+bad.getActivitySubCategory(),bad.getActivitySubCategory().equals(ActivitySubCategoryCode.ASC_1));
+        assertTrue("ActivityType error : Expected : {}, founded : "+bad.getActivityType(),bad.getActivityType().equals(ActivityTypeCode.AT_1));
+        assertTrue("ActivitySource error : Expected : {}, founded : "+bad.getActivitySource(),bad.getActivitySource().equals(new ActivitySourceCode("AS_1")));
+        assertTrue("ActivityOwnership error : Expected : {}, founded : "+bad.getActivityOwnership(),bad.getActivityOwnership().equals(true));
+        assertTrue("Unit error : Expected : {}, founded : "+bad.getUnit().getSymbol(),bad.getUnit().getUnitCode().equals(UnitCode.U5321));
+        */
+    }
+
+    private boolean around(Double value1,Double value2){
+        if(value1>=value2*(1-ERROR_MARGE) && value1 <= value2*(1+ERROR_MARGE)){
+            return true;
+        }
+       return false;
+    }
+}
