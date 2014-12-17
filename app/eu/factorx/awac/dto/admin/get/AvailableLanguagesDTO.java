a7ce9593539601f76e4478bce71622d358f9d009
jcarton <jerome.carton@factorx.eu>
2014-12-17 11:27:35 +0100

    Minor changes related to ui translations initialization

--- app/eu/factorx/awac/dto/admin/get/AvailableLanguagesDTO.java
+++ app/eu/factorx/awac/dto/admin/get/AvailableLanguagesDTO.java
@@ -5,51 +5,48 @@ import eu.factorx.awac.dto.validation.annotations.NotNull;
 
 public class AvailableLanguagesDTO extends DTO {
 
-    private boolean frEnabled = false;
-    private boolean nlEnabled = false;
-    private boolean enEnabled = false;
-
-	public AvailableLanguagesDTO() {
-	}
+	private boolean frEnabled;
+	private boolean nlEnabled;
+	private boolean enEnabled;
 
 	public AvailableLanguagesDTO(boolean frEnabled, boolean nlEnabled, boolean enEnabled) {
-        this.frEnabled = frEnabled;
-        this.nlEnabled = nlEnabled;
-        this.enEnabled = enEnabled;
-    }
+		this.frEnabled = frEnabled;
+		this.nlEnabled = nlEnabled;
+		this.enEnabled = enEnabled;
+	}
 
-    public boolean isFrEnabled() {
-        return frEnabled;
-    }
+	public boolean isFrEnabled() {
+		return frEnabled;
+	}
 
-    public void setFrEnabled(boolean frEnabled) {
-        this.frEnabled = frEnabled;
-    }
+	public void setFrEnabled(boolean frEnabled) {
+		this.frEnabled = frEnabled;
+	}
 
-    public boolean isNlEnabled() {
-        return nlEnabled;
-    }
+	public boolean isNlEnabled() {
+		return nlEnabled;
+	}
 
-    public void setNlEnabled(boolean nlEnabled) {
-        this.nlEnabled = nlEnabled;
-    }
+	public void setNlEnabled(boolean nlEnabled) {
+		this.nlEnabled = nlEnabled;
+	}
 
-    public boolean isEnEnabled() {
-        return enEnabled;
-    }
+	public boolean isEnEnabled() {
+		return enEnabled;
+	}
 
-    public void setEnEnabled(boolean enEnabled) {
-        this.enEnabled = enEnabled;
-    }
+	public void setEnEnabled(boolean enEnabled) {
+		this.enEnabled = enEnabled;
+	}
 
-    @Override
-    public String toString() {
-        return "AvailableLanguagesDTO{" +
-                "frEnabled=" + frEnabled +
-                ", nlEnabled=" + nlEnabled +
-                ", enEnabled=" + enEnabled +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "AvailableLanguagesDTO{" +
+			"frEnabled=" + frEnabled +
+			", nlEnabled=" + nlEnabled +
+			", enEnabled=" + enEnabled +
+			'}';
+	}
 }
 
 
