293921960f73c0826cbfcd60b7851bee1627ee52
xaviermarichal <xavier.marichal@factorx.eu>
2015-03-09 09:41:12 +0100

    procedure for copy of Accept to Dev

---
+++ migrations/copyAccept2Dev/resetPasswordsEmails.sql
@@ -0,0 +1,4 @@
+update account set password = 'f59a70b5b3911c933905e14402e99f75b096c115a98f05a36fada5f04edd5ef604daa5710de8a018';
+select * from account;
+update account set email = id||'noreply@noorg.com';
+select * from account;
